490d73755012db027ad3a8e71faac20797f82c32
kpost34 <kpost342005@hotmail.com>
2022-09-29 22:41:34 -0600

    Functionalized ggplot commands and added faceted plots to EDA

--- code/02_EDA.R
+++ code/02_EDA.R
@@ -1,87 +1,162 @@
 #Created by Keith Post on 9/22/22
 #EDA
 
-#### Load Packages==================================================================================
-pacman::p_load(here,tidyverse,skimr,naniar)
+#### Load Packages and Source DFs====================================================================
+pacman::p_load(here,tidyverse,scales,GGally)
+source(here("code","01_DataSetup.R"))
+source(here("code","02a_EDA.R"))
 
 
 #### Data Import====================================================================================
 ### Set path
-BMI_tidy_filename<-dir(path=here("data","tidy_data"),pattern=paste0("alpine_bmi_env_n_",".+",".rds")) %>%
-  sort() %>%
-  tail(n=1)
-BMIenvDF_path<-here("data","tidy_data",BMI_tidy_filename)
+# BMI_tidy_filename<-dir(path=here("data","tidy_data"),pattern=paste0("alpine_bmi_env_n_",".+",".rds")) %>%
+#   sort() %>%
+#   tail(n=1)
+# BMIenvDF_path<-here("data","tidy_data",BMI_tidy_filename)
 
 ### Read in data
-BMIenvDF<-readRDS(BMIenvDF_path)
-
-
-
-#### Data Checking==================================================================================
-### Basic information
-dim(BMIenvDF)
-summary(BMIenvDF)
-glimpse(BMIenvDF)
-skim(BMIenvDF)
-
-### Missing information
-miss_case_summary(BMIenvDF) %>% distinct(n_miss,pct_miss) #none
-miss_var_table(BMIenvDF) #none
-miss_case_table(BMIenvDF) #none
+# BMIenvDF<-readRDS(BMIenvDF_path)
 
 
 ##### Exploratory Data Analysis=====================================================================
-#### Taxonomic data
-### Location-counts
-BMIenvDF %>%
-  group_by(location) %>%
-  summarize(count=sum(count)) %>%
-  mutate(location=fct_reorder(location,count,.fun=sum,.desc=TRUE)) %>%
-  ggplot(aes(x=location,y=count)) +
-  geom_col(color="black",fill="steelblue") +
-  scale_y_continuous(expand=expansion(mult=c(0,0.1))) +
-  theme_bw() 
+#### Taxonomic data---------------------------------------------------------------------------------
+### Location----------------------------------------------------------------------------------------
+## Total counts
+barplotter_tot(BMIcountTidyDF,location,count)
 
-## Order-counts
-BMIenvDF %>%
-  group_by(order) %>%
-  summarize(count=sum(count)) %>%
-  mutate(order=fct_reorder(order,count,.fun=sum,.desc=TRUE)) %>%
-  ggplot(aes(x=order,y=count)) +
-  geom_col(color="black",fill="steelblue") +
-  scale_y_continuous(expand=expansion(mult=c(0,0.1))) +
+## Average counts
+# Bar plot
+barplotter_avg(BMIcountTidyDF,location,count) 
+
+## Counts
+# Boxplot
+boxplotter(BMIcountTidyDF,location,count)
+
+
+### local_site--------------------------------------------------------------------------------------
+## Total counts
+barplotter_tot(BMIcountTidyDF,local_site,count)
+
+## Average counts
+# Bar plot
+barplotter_avg(BMIcountTidyDF,local_site,count) 
+
+## Counts
+# Boxplot
+boxplotter(BMIcountTidyDF,local_site,count)
+
+
+
+### Order-------------------------------------------------------------------------------------------
+## Total counts
+barplotter_tot(BMIcountTidyDF,order,count,angled=TRUE)
+
+## Average counts
+barplotter_avg(BMIcountTidyDF,order,count,angled=TRUE)
+
+
+### Family
+## Total counts
+barplotter_tot(BMIcountTidyDF,family,count,angled=TRUE)
+
+
+### Average counts
+barplotter_avg(BMIcountTidyDF,family,count,angled=TRUE)
+
+
+### Order-Location----------------------------------------------------------------------------------
+## Average counts
+# Stacked
+barplotter_avg2(BMIcountTidyDF,order,location,count,angled=TRUE)
+
+# Grouped
+barplotter_avg2(BMIcountTidyDF,order,location,count,pos="dodge",angled=TRUE)
+
+
+### Order-local_site--------------------------------------------------------------------------------
+# Stacked
+barplotter_avg2(BMIcountTidyDF,order,local_site,count,angled=TRUE)
+
+# Grouped
+barplotter_avg2(BMIcountTidyDF,order,local_site,count,pos="dodge",angled=TRUE)
+
+
+
+#### Environmental data-----------------------------------------------------------------------------
+### Faceted barplot of quantitative variables
+## By location
+BMIenvTidyDF %>%
+  group_by(location,variable) %>%
+  summarize(mean_value = mean(value)) %>% 
+  ungroup() %>%
+  mutate(location = fct_reorder(location,mean_value,.fun=sum,.desc=TRUE)) %>%
+  ggplot(aes(x=location,y=mean_value,fill=variable)) +
+  geom_col(color="black") +
+  scale_fill_viridis_d() +
+  #geom_errorbar(aes(ymin=lower,ymax=upper),width=0.4) +
+  #scale_y_continuous(expand=expansion(mult=c(0,0.1))) +
   theme_bw() +
-  theme(axis.text.x=element_text(vjust=grid::unit(c(rep(c(-2,0),3),-2),"points")))
+  theme(axis.text.x=element_text(angle=45,vjust=0.5,hjust=0.5)) +
+  facet_wrap(~variable,scales="free")
 
-### Location-Order
-BMIenvDF %>%
-  group_by(order) %>%
-  summarize(count=sum(count),
-            location=location) %>%
-  distinct() %>%
-  mutate(order=fct_reorder(order,count,.fun=sum,.desc=TRUE)) %>%
-  ggplot(aes(x=order,y=count)) +
-  geom_col(color="black",aes(fill=location)) +
-  scale_y_continuous(expand=expansion(mult=c(0,0.1))) +
-  theme_bw() 
-
-
-## Family-counts
-BMIenvDF %>%
-  group_by(family) %>%
-  summarize(count=sum(count)) %>%
-  mutate(order=fct_reorder(family,count,.fun=sum,.desc=TRUE)) %>%
-  ggplot(aes(x=order,y=count)) +
-  geom_col(color="black",fill="steelblue") +
-  scale_y_continuous(expand=expansion(mult=c(0,0.1))) +
+## By local_site
+BMIenvTidyDF %>%
+  group_by(local_site,variable) %>%
+  summarize(mean_value = mean(value)) %>% 
+  ungroup() %>%
+  mutate(location = fct_reorder(local_site,mean_value,.fun=sum,.desc=TRUE)) %>%
+  ggplot(aes(x=location,y=mean_value,fill=variable)) +
+  geom_col(color="black") +
+  scale_fill_viridis_d() +
+  #geom_errorbar(aes(ymin=lower,ymax=upper),width=0.4) +
+  #scale_y_continuous(expand=expansion(mult=c(0,0.1))) +
   theme_bw() +
-  theme(axis.text.x=element_text(angle=75,vjust=0.5))
+  theme(axis.text.x=element_text(angle=45,vjust=0.5,hjust=0.5)) +
+  facet_wrap(~variable,scales="free")
 
 
-###
-  
+### Shore
+## By location
+BMIenvTidyDF %>%
+  group_by(location,shore) %>%
+  summarize(total_count = n()) %>% 
+  mutate(location = fct_reorder(location,total_count,.fun=sum,.desc=TRUE)) %>%
+  ggplot(aes(x=location,y=total_count,fill=shore)) +
+  geom_col(color="black") +
+  scale_fill_viridis_d() +
+  #geom_errorbar(aes(ymin=lower,ymax=upper),width=0.4) +
+  #scale_y_continuous(expand=expansion(mult=c(0,0.1))) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=45,vjust=0.5,hjust=0.5))
+
+
+## By local_site
+BMIenvTidyDF %>%
+  group_by(local_site,shore) %>%
+  summarize(mean_value = mean(value)) %>% 
+  ungroup() %>%
+  mutate(location = fct_reorder(local_site,mean_value,.fun=sum,.desc=TRUE)) %>%
+  ggplot(aes(x=local_site,y=mean_value,fill=shore)) +
+  geom_col(color="black") +
+  scale_fill_viridis_d() +
+  #geom_errorbar(aes(ymin=lower,ymax=upper),width=0.4) +
+  #scale_y_continuous(expand=expansion(mult=c(0,0.1))) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=45,vjust=0.5,hjust=0.5)) +
+  facet_wrap(~shore,scales="free")
+
+
+###  
 
 
 
+
+## Need to commit
+  # functionalized ggplots
+
+## Need to do
+  #make function (or update current functions) for faceted plots
+  #pairwise plots for all quantitative (continuous) env vars
+  #look at assumptions of PCA and see if transforms needed--distribution (symmetrical), multicollinearity
 
                 
