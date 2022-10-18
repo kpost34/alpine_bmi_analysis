af12614e2d8939c4e0b443fdd87296a8627b33e8
kpost34 <kpost342005@hotmail.com>
2022-10-17 23:21:09 -0600

    Began PCA of env vars and omputed stats and made biplots of PCAs using prcomp and princomp and prcomp with and without nitrate

--- code/03_PCA.R
+++ code/03_PCA.R
@@ -3,14 +3,15 @@
 
 
 #### Source DFs (and load packages)=================================================================
-pacman::p_load(here,rstatix,MASS)
+pacman::p_load(here,rstatix,MASS,GGally,vegan,ggfortify,ggbiplot)
 source(here("code","01_DataSetup.R"))
+source(here("code","02a_EDA_functions.R"))
 source(here("code","03a_PCA_functions.R"))
 
 select<-dplyr::select
 
 
-##### Test Assumptions of PCA========================================================================
+##### Test Assumptions of PCA=======================================================================
 #### Assumption 1: Distributions of all variables follow a normal distribution (or are at least 
   #symmetrical)
 
@@ -35,7 +36,7 @@ BMIenvWideDF %>%
 
 
 ### Transformations (using BoxCox)
-## Transform data
+## Transform data (5/6 quantitative vari)
 # Wide format
 BMIenvWideDF %>%
   mutate(across(c(elevation,temp,DO:nitrate),~boxcoxer(.x),.names="{.col}_trans")) %>%
@@ -108,13 +109,13 @@ BMIenvWideDF_trans2 %>%
 
 
 #### Assumption 2: Any systematic relationships between variables are linear
-### Non-transformed
+### Transformed
 BMIenvWideDF_trans %>% 
   select(sat,ends_with("_trans")) %>%
   ggpairs(lower=list(continuous=wrap(smoother))) +
   theme_bw()
 
-### Transformed
+### Non-transformed
 BMIenvWideDF %>% 
   select(elevation:nitrate) %>%
   ggpairs(lower=list(continuous=wrap(smoother))) +
@@ -127,20 +128,125 @@ BMIenvWideDF %>%
 
 
 
+##### PCA===========================================================================================
+### Run PCA
+## prcomp
+BMIenvWideDF_trans %>%
+  #select sites + env vars
+  select(project_site,sat,elevation_trans:last_col()) %>%
+  #formula input using scaled and centered variables
+  prcomp(~sat + elevation_trans + temp_trans + DO_trans + ph_trans + nitrate_trans,
+         data=.,scale.=TRUE,center=TRUE) -> envPCA1
+
+
+## princomp
+BMIenvWideDF_trans %>%
+  #select sites + env vars
+  select(project_site,sat,elevation_trans:last_col()) %>%
+  #formula input using correlation matrix
+  princomp(~sat + elevation_trans + temp_trans + DO_trans + ph_trans + nitrate_trans,
+           data=.,cor=TRUE,scores=TRUE,fix_sign=TRUE) -> envPCA2
+
+
+
+
+ ### Display output (statistics)
+## sds
+envPCA1
+envPCA2
+
+## sds, prop of var, and cumulative proportion
+summary(envPCA1)
+summary(envPCA2)
+#PC1 = ~ 49% of variance; PC2 = ~ 25% of variance
+
+## isolate sds
+envPCA1$sdev
+envPCA2$sdev
+
+
+## EVs
+envPCA1$sdev^2
+envPCA2$sdev^2
+
+## loadings
+envPCA1$rotation
+envPCA2$loadings
+#PC1: negative: sat & elevation; positive: temp & DO
+#PC2: negative: nitrate, pH, & temp
+
+## scores
+envPCA1$x
+envPCA2$scores
+
+
+### Choosing PCs
+## Scree plot
+screeplot(envPCA1,bstick=TRUE,type="barplot",
+          main="PCA of alpine lakes environmental variables")
+abline(h=mean(envPCA1$sdev^2))
+#1) EVs drop the most from PC1 to PC2
+#2) PC1-PC3 above mean EV
+#3) PC1-PC3 above broken stick distribution
+
+
+### Visualize biplot
+## Base R
+biplot(envPCA1)
+
+#ggbiplot
+ggbiplot(envPCA1) +
+  theme_bw()
+
+## ggplot with ggfortify
+autoplot(envPCA1,loadings=TRUE,loadings.label=TRUE,shape=FALSE,
+         loadings.label.repel=TRUE) +
+  expand_limits(x=c(-0.5,0.5)) +
+  theme_bw()
+
+
+
+
+
+
+### Run PCA (after removing nitrate)
+## prcomp
+BMIenvWideDF_trans %>%
+  #select sites + env vars
+  select(project_site,sat,elevation_trans:ph_trans) %>%
+  #formula input using scaled and centered variables
+  prcomp(~sat + elevation_trans + temp_trans + DO_trans + ph_trans,
+         data=.,scale.=TRUE,center=TRUE) -> envPCA3
+
+summary(envPCA3)
+
+screeplot(envPCA3,bstick=TRUE,type="barplot",
+          main="PCA of alpine lakes environmental variables")
+abline(h=mean(envPCA3$sdev^2))
+
+envPCA1$rotation
+envPCA3$rotation
+
+biplot(envPCA1)
+biplot(envPCA3)
+
+
 #----------------------------------------------------------------------------------------------
 
 ## DONE
+#began PCA of env vars
+#computed stats and made biplots of PCAs using prcomp and princomp and prcomp with and without
+  #nitrate
+
+
+
+## LAST COMMIT
 #created boxcox and inverse transformation functions
 #completed testing of assumption 1
 #created and implemented new functions histogrammer nad qqplotter
 #tested assumption 2
 
+## TO DO
+#consider doing some plots of diversity (family richness, index)
 
-
-## LAST COMMIT
-#constructed heat map all continuous variables and scatterplots of all continuous variables, either
-#as is or using groups
-#testing assumptions of PCA
-#renamed EDA function script
-#created new script for PCA functions
 
